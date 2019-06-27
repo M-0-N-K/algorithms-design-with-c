291c655695ed81bcb5f0fbdd187f487b583841c3
M-O-N-K <jagdeepak009@gmail.com>
2019-06-27 15:05:24 +0530

    insertionsort.c added

---
+++ insertionsort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+void main(){
+	int array[10]={10,20,3,4,8,99,65,34,86,38};
+	int i,j,key;
+
+	printf("\n unsorted array :  \n");
+	for(i=0;i<10;i++)
+		printf("%d  ",array[i]);
+
+	for(j=1;j<10;j++){
+		key=array[j];
+		i=j-1;
+		while(i>=0 && array[i]>key){
+			array[i+1]=array[i];
+			i=i-1;
+		}
+		array[i+1]=key;
+	}
+
+	printf("\n sorted array\n");
+	for(i=0;i<10;i++)
+		printf("%d  ",array[i]);
+}
